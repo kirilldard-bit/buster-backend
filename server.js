a51598aaa7e3394379a64859613059224a4fa549
kirilldard-bit <kirilldard@gmail.com>
2026-05-13 18:28:40 +0300

    first backend commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+  res.json({
+    status: 'BUSTER backend running'
+  });
+
+});
+
+app.get('/check-access', (req, res) => {
+
+  const telegramId =
+    req.query.user;
+
+  console.log(
+    'Checking access for:',
+    telegramId
+  );
+
+  res.json({
+
+    access: false,
+
+    user: telegramId,
+
+    subscription: null
+
+  });
+
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
+});
