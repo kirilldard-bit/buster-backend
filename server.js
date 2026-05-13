5437ba55e7aa8c5dcb031745224dd9fc5aaf5c74
kirilldard-bit <kirilldard@gmail.com>
2026-05-13 18:59:52 +0300

    fixed railway port

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/check-access', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
 
   console.log(
     `Server running on port ${PORT}`
